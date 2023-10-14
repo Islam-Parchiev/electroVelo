163d8e7c105ce029a045cdad2929197e176a9a3a
Vedar <islamparchev006@gmail.com>
2023-10-14 18:29:52 +0300

    start create slider

--- client/src/components/MainAbout/MainAbout.tsx
+++ client/src/components/MainAbout/MainAbout.tsx
@@ -1,10 +1,30 @@
 import {FC} from 'react'
 
+
 import styles from './MainAbout.module.scss';
+
 
 const MainAbout:FC = () => {
 	return (
-		<section className={styles.MainAbout}>MainAbout</section>
+		<section className={styles.MainAbout}>
+	     <div className={`container ${styles.MainAbout__container}`}>
+				<div className={styles.MainAbout__slider}>
+					 <div className={styles.MainAbout__slides}>
+						<div className={styles.MainAbout__slide}>
+							<div className={styles.MainAbout__text}>
+								<h1 className={styles.MainAbout__title}>Электро велосипеды</h1>
+								<p className={styles.MainAbout__descr}>
+							Cento10 Hybrid — это гоночный велосипед с помогающим педалированию электроприводом, 
+							который устанавливает новый, очень высокий стандарт для данной категории
+								</p>
+							
+        		  </div>
+							<button className={`btn-reset btn ${styles.MainAbout__btn}`}>Подробнее</button>
+						</div>
+					 </div>
+				</div>
+			 </div>
+		</section>
 	)
 }
 
