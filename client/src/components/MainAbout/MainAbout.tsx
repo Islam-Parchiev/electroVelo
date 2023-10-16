682bde9d7317dbaa0b86c77f5f9f442c7fbc497c
Vedar <islamparchev006@gmail.com>
2023-10-16 10:41:06 +0300

    start create mainAboutInfo

--- client/src/components/MainAbout/MainAbout.tsx
+++ client/src/components/MainAbout/MainAbout.tsx
@@ -1,29 +1,119 @@
-import {FC} from 'react'
+import { FC } from 'react'
 
-import styles from './MainAbout.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, A11y } from 'swiper/modules'
 
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import styles from './MainAbout.module.scss'
 
-
-const MainAbout:FC = () => {
+const MainAbout: FC = () => {
 	return (
 		<section className={styles.MainAbout}>
-	     <div className={`container ${styles.MainAbout__container}`}>
-				<div className={styles.MainAbout__slider}>
-					 <div className={styles.MainAbout__slides}>
-						<div className={styles.MainAbout__slide}>
-							<div className={styles.MainAbout__text}>
-								<h1 className={styles.MainAbout__title}>Электро велосипеды</h1>
-								<p className={styles.MainAbout__descr}>
-							Cento10 Hybrid — это гоночный велосипед с помогающим педалированию электроприводом, 
-							который устанавливает новый, очень высокий стандарт для данной категории
-								</p>
-							
-        		  </div>
-							<button className={`btn-reset btn ${styles.MainAbout__btn}`}>Подробнее</button>
+			<div className={styles.MainAbout__top}>
+				<Swiper
+					className={styles.MainAbout__slider}
+					slidesPerView={1}
+					modules={[Pagination, Navigation, A11y]}
+					navigation
+					pagination={{ clickable: true }}
+					scrollbar={{ draggable: true }}
+					onSwiper={(swiper: any) => console.log(swiper)}
+					onSlideChange={() => console.log('slide change')}>
+					<SwiperSlide>
+						<div className={styles.Slide}>
+							<div className={styles.Slide__content}>
+								<div className={styles.MainAbout__text}>
+									<h1 className={styles.MainAbout__title}>
+										Электро велосипеды
+									</h1>
+									<p className={styles.MainAbout__descr}>
+										Cento10 Hybrid — это гоночный велосипед
+										с помогающим педалированию
+										электроприводом, который устанавливает
+										новый, очень высокий стандарт для данной
+										категории
+									</p>
+								</div>
+								<button
+									className={`btn-reset btn ${styles.MainAbout__btn}`}>
+									Подробнее
+								</button>
+							</div>
 						</div>
-					 </div>
-				</div>
-			 </div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className={styles.Slide}>
+							<div className={styles.Slide__content}>
+								<div className={styles.MainAbout__text}>
+									<h1 className={styles.MainAbout__title}>
+										Электро велосипеды
+									</h1>
+									<p className={styles.MainAbout__descr}>
+										Cento10 Hybrid — это гоночный велосипед
+										с помогающим педалированию
+										электроприводом, который устанавливает
+										новый, очень высокий стандарт для данной
+										категории
+									</p>
+								</div>
+								<button
+									className={`btn-reset btn ${styles.MainAbout__btn}`}>
+									Подробнее
+								</button>
+							</div>
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className={styles.Slide}>
+							<div className={styles.Slide__content}>
+								<div className={styles.MainAbout__text}>
+									<h1 className={styles.MainAbout__title}>
+										Электро велосипеды
+									</h1>
+									<p className={styles.MainAbout__descr}>
+										Cento10 Hybrid — это гоночный велосипед
+										с помогающим педалированию
+										электроприводом, который устанавливает
+										новый, очень высокий стандарт для данной
+										категории
+									</p>
+								</div>
+								<button
+									className={`btn-reset btn ${styles.MainAbout__btn}`}>
+									Подробнее
+								</button>
+							</div>
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className={styles.Slide}>
+							<div className={styles.Slide__content}>
+								<div className={styles.MainAbout__text}>
+									<h1 className={styles.MainAbout__title}>
+										Электро велосипеды
+									</h1>
+									<p className={styles.MainAbout__descr}>
+										Cento10 Hybrid — это гоночный велосипед
+										с помогающим педалированию
+										электроприводом, который устанавливает
+										новый, очень высокий стандарт для данной
+										категории
+									</p>
+								</div>
+								<button
+									className={`btn-reset btn ${styles.MainAbout__btn}`}>
+									Подробнее
+								</button>
+							</div>
+						</div>
+					</SwiperSlide>
+				</Swiper>
+			</div>
+			<div className={styles.MainAbout__bottom}>
+				
+			</div>
 		</section>
 	)
 }
