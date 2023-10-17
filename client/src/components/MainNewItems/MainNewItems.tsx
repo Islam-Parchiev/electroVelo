f6af8ee9ce47bb5830409aa8df802247165e9c77
Vedar <islamparchev006@gmail.com>
2023-10-17 10:25:55 +0300

    start create Card

--- client/src/components/MainNewItems/MainNewItems.tsx
+++ client/src/components/MainNewItems/MainNewItems.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { FC } from 'react'
 
-const MainNewItems = () => {
+import styles from './MainNewItems.module.scss'
+const MainNewItems: FC = () => {
 	return (
-		<div>MainNewItems</div>
+		<section className={styles.MainNewItems}>
+			<div className={`container ${styles.MainNewItems__container}`}>
+				<h2 className={styles.MainNewItems__title}>Новинки</h2>
+				<ul className={`list-reset ${styles.MainNewItems__cards}`}>
+					<li className={styles.Card}>
+						<div className={styles.Card__header}>
+							<div className={styles.Card__country}>
+								<img src="/images/Card/country1.png" alt="" />
+							</div>
+							<div className={styles.Card__status}>
+								<span>В наличии</span>
+							</div>
+						</div>
+						<div className={styles.Card__body}></div>
+						<div className={styles.Card__footer}></div>
+					</li>
+				</ul>
+			</div>
+		</section>
 	)
 }
 
--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -3,12 +3,14 @@ import {FC} from 'react'
 
 import MainAbout from '../../components/MainAbout/MainAbout'
 import Brands from '../../components/Brands/Brands'
+import MainNewItems from '../../components/MainNewItems/MainNewItems'
 
 const Main:FC = () => {
 	return (
 		<main className="Main">
 			<MainAbout/>
 			<Brands/>
+			<MainNewItems/>
 		</main>
 	)
 }
