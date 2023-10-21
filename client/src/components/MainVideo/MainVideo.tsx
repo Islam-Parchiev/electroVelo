dccde3fe3311cc2a002f6276552bdb029a41d774
Vedar <islamparchev006@gmail.com>
2023-10-21 14:26:45 +0300

    start create MainVideo section

---
+++ client/src/components/MainVideo/MainVideo.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react'
+
+import styles from './MainVideo.module.scss'
+const MainVideo:FC = () => {
+	return (
+		<section className={styles.MainVideo}>
+			<div className={`container ${styles.MainVideo__container}`}>
+      	<div className={styles.MainVideo__header}>
+
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default MainVideo
