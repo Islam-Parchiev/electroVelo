617cfaee3f183909c3e4b0015a28d6e5ca26c702
Vedar <islamparchev006@gmail.com>
2023-11-11 18:09:46 +0300

    fix switchCheckbox

--- client/src/components/Filters/Filters.tsx
+++ client/src/components/Filters/Filters.tsx
@@ -7,10 +7,11 @@ import styles from './Filters.module.scss'
 interface FiltersProps {}
 
 const Filters:React.FC<FiltersProps> = (props) => {
+	const [checked,setChecked]= React.useState<boolean>(false);
 	const {} = props;
 	return (
 		<div className={styles.Filters}>
-			<SwitchCheckbox/>
+			<SwitchCheckbox value={checked} text="Только в наличии" handleClick={setChecked}/>
 		</div>
 	)
 }
--- client/src/components/SwitchCheckbox/SwitchCheckbox.tsx
+++ client/src/components/SwitchCheckbox/SwitchCheckbox.tsx
@@ -2,16 +2,27 @@ import React from 'react'
 
 import styles from './SwitchCheckbox.module.scss'
 
-interface SwitchCheckboxProps {}
+interface SwitchCheckboxProps {
+	text:string;
+	value:boolean;
+	handleClick:(value:boolean)=>void;
+
+}
 
 const SwitchCheckbox:React.FC<SwitchCheckboxProps> = (props) => {
-	const {} = props;
-	const [checked,setChecked] =React.useState<boolean>(false);
-	console.log(checked);
+	const {
+		text,
+		value,
+		handleClick,
+	} = props;
+console.log(value);
 	return (
-		<label className={styles.checkboxIos}>
-			<input type="checkbox" checked={checked} onClick={()=> setChecked(!checked)}/>
-			<span className={styles.checkboxIosSwitch}></span>
+		<label className={styles.switchCheckbox}>
+			<span className={styles.switchCheckbox__text}>
+				{text}
+			</span>
+			<input type="checkbox" checked={value} onClick={()=> handleClick(!value)}/>
+			<span className={styles.switchCheckbox__switch}></span>
 		</label>
 	)
 }
