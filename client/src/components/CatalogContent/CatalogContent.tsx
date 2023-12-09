39ee6864396c0640cf2fe583484acd327eebccc6
Vedar <islamparchev006@gmail.com>
2023-12-09 10:51:13 +0300

    add: useCatalog

--- client/src/components/CatalogContent/CatalogContent.tsx
+++ client/src/components/CatalogContent/CatalogContent.tsx
@@ -1,7 +1,4 @@
-import React, { useState } from 'react'
-
-import { useQuery } from '@tanstack/react-query'
-import { useAppSelector } from '@redux/store'
+import React from 'react'
 
 import Filters from '@components/Filters/Filters'
 import CatalogSort from '@components/CatalogSort/CatalogSort'
@@ -9,22 +6,15 @@ import CatalogView from '@components/CatalogView/CatalogView'
 import Card from '@components/Card/Card'
 import Pagination from '@components/Pagination/Pagination'
 
-import productService  from '@services/product.service'
+
 import { ICard } from 'Card'
+
+import { useCatalog } from './useCatalog'
 
 import styles from './CatalogContent.module.scss'
 
-const CatalogContent:React.FC = () => {
-	const [page,setPage] = useState(1);
-	const sortProperty = useAppSelector((state)=> state.sort.sortProperty);
-	const filtersValue = useAppSelector((state)=> state.filters);
-	const categoriesValue = useAppSelector((state)=> state.filters.selectedCategories);
-	const materialsValue = useAppSelector((state)=>state.filters.selectedMaterials);
-	const query = useQuery<{data:any}>({ queryKey: ['catalogProducts',page,sortProperty,filtersValue.available,categoriesValue,materialsValue], 
-		queryFn:()=> productService.getProductsByFilters(page,6,sortProperty,filtersValue.available.toString(),categoriesValue,materialsValue)})
-	
-	console.log(page);
-	console.log(query.data?.data);
+const CatalogContent: React.FC = () => {
+	const { products, totalPages, page, setPage, isLoading, isSuccess } = useCatalog()
 	return (
 		<section className={styles.CatalogContent}>
 			<div className="container">
@@ -38,22 +28,32 @@ const CatalogContent:React.FC = () => {
 						<div className={styles.CatalogGoods}>
 							<ul
 								className={`list-reset ${styles.CatalogGoods__list}`}>
-								{query.data?.data.data.map((item: ICard) => (
-									<Card
-										type="secondary"
-										id={item.id}
-										otherClass={styles.CatalogGoods__card}
-										price={item.price}
-										title={item.title}
-										available={item.available}
-										images={item.previewImage}
-										countrySrc={item.country}
-										key={item.id}
-									/>
-								))}
+								{isLoading
+									? 'Loading...'
+									: isSuccess
+										? products.map((item: ICard) => (
+											<Card
+												type="secondary"
+												id={item.id}
+												otherClass={
+													styles.CatalogGoods__card
+												}
+												price={item.price}
+												title={item.title}
+												available={item.available}
+												images={item.previewImage}
+												countrySrc={item.country}
+												key={item.id}
+											/>
+									  ))
+										: 'Error'}
 							</ul>
 						</div>
-						<Pagination totalPages={query.data?.data.totalPages} setPage={setPage} page={page}/>
+						<Pagination
+							totalPages={totalPages}
+							setPage={setPage}
+							page={page}
+						/>
 					</div>
 				</div>
 			</div>
---
+++ client/src/components/CatalogContent/useCatalog.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import { useAppSelector } from '@redux/store'
+import { useQuery } from '@tanstack/react-query'
+import productService from '@services/product.service'
+import { ICard } from 'Card'
+
+export function useCatalog() {
+	const [page, setPage] = useState(1)
+	const sortProperty = useAppSelector(state => state.sort.sortProperty)
+	const availableValue = useAppSelector(state => state.filters.available)
+	const categoriesValue = useAppSelector(state => state.filters.selectedCategories)
+	const materialsValue = useAppSelector(state => state.filters.selectedMaterials)
+	const {data,isLoading,isSuccess} = useQuery<{ data: any }>({
+		queryKey: [
+			'catalogProducts',
+			page,
+			sortProperty,
+			availableValue,
+			categoriesValue,
+			materialsValue,
+		],
+		queryFn: () =>
+			productService.getProductsByFilters(
+				page,
+				6,
+				sortProperty,
+				availableValue.toString(),
+				categoriesValue,
+				materialsValue,
+			),
+	})
+	const products:ICard[] =data?.data.data;
+	const totalPages:number = data?.data.totalPages;
+	return {products,totalPages,page,setPage,isLoading,isSuccess}
+}
