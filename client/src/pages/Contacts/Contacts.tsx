64419e7dde65b500c7afece38add3392a3fff3e0
Vedar <islamparchev006@gmail.com>
2024-01-03 15:01:29 +0300

    add:ContactsQuestions

--- client/src/pages/Contacts/Contacts.tsx
+++ client/src/pages/Contacts/Contacts.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import Header from '@components/Header/Header';
 import ContactsMap from '@components/ContactsMap/ContactsMap';
 import ContactsInfo from '@components/ContactsInfo/ContactsInfo';
+import ContactsForm from '@components/ContactsForm/ContactsForm';
+import ContactsQuestions from '@components/ContactsQuestions/ContactsQuestions';
+import Subscribe from '@components/Subscribe/Subscribe';
+import Brands from '@components/Brands/Brands';
 
 import styles from './Contacts.module.scss';
 
@@ -13,6 +17,10 @@ const Contacts:React.FC = () => {
 			<main className={styles.Contacts}>
 				<ContactsMap/>
 				<ContactsInfo/>
+				<ContactsForm/>
+				<ContactsQuestions/>
+				<Brands/>
+				<Subscribe/>
 			</main>
 		</>
 	)
