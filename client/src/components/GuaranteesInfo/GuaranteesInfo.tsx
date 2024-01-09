e34ec4eb92af272f33e374acecc229a6e6bf8bd1
Vedar <islamparchev006@gmail.com>
2024-01-09 14:54:04 +0300

    add:GuaranteesInfo

--- client/src/components/GuaranteesInfo/GuaranteesInfo.tsx
+++ client/src/components/GuaranteesInfo/GuaranteesInfo.tsx
@@ -7,9 +7,90 @@ interface GuaranteesInfoProps {}
 const GuaranteesInfo:React.FC<GuaranteesInfoProps> = (props) => {
 	const {} = props;
 	return (
-		<div className={styles.GuaranteesInfo}>
-			GuaranteesInfo Component
-		</div>
+		<section className={styles.GuaranteesInfo}>
+
+			<div className="container">
+				<h2 className={styles.GuaranteesInfo__title}>
+				Гарантия на бренды
+				</h2>
+				<div className={styles.GuaranteesInfo__inner}>
+					<div className={styles.GuaranteesInfo__column}>
+						<ul className={`list-reset ${styles.GuaranteesInfo__list}`}>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+
+						</ul>
+					</div>
+					<div className={styles.GuaranteesInfo__column}>
+						<ul className={`list-reset ${styles.GuaranteesInfo__list}`}>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							<li className={styles.GuaranteesInfo__item}>
+								<span className={styles.GuaranteesInfo__item_title}>
+								BMC
+								</span>
+								<span className={styles.GuaranteesInfo__item_price}>
+								2 года гарантия +1 год при регистрации на сайте
+								</span>
+							</li>
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
 	)
 }
 
